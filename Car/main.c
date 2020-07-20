109cb806f2c933afbc1837a9bc64b6cc7dbb590b
Chinh Nguyen <22429215+tchinh97@users.noreply.github.com>
2020-07-20 22:45:57 +0700

    Initial Commit

---
+++ Car/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Car
+//
+//  Created by Chinh Nguyen on 7/20/20.
+//  Copyright © 2020 Chinh Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
