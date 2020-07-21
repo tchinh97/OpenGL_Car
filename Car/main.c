b4104c5f1d7af78b2d8879b0d517b9a1ad08ee75
Chinh Nguyen <22429215+tchinh97@users.noreply.github.com>
2020-07-21 09:32:30 +0700

    hình chữ nhật quay quanh trục z

--- Car/main.c
+++ Car/main.c
@@ -3,34 +3,68 @@
 #include <OpenGL/glu.h>
 #include <OpenGL/gl.h>
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0); //    màu của đối tượng
-//    Thong tin cua hinh chu nhat
-    glBegin(GL_POLYGON);
-        glVertex3f(0.25, 0.25, 0.0);
-        glVertex3f(0.75, 0.25, 0.0);
-        glVertex3f(0.75, 0.75, 0.0);
-        glVertex3f(0.25, 0.75, 0.0);
-    glEnd();
-    glFlush();
-}
+//Góc quay của hình chữ nhật
+static GLfloat goc = 0.0;
 
 void init() {
-    glClearColor(0.0, 0.0, 0.0, 0.0); //    nền đen
+//    Nền màu đen
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+    
+//    Xoay quanh trục z
+    glRotatef(goc, 0.0, 0.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glRectf(-25.0, -25.0, 25.0, 25.0); /* vẽ hcn */
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void spinDisplay(){
+    goc = goc + 2.0;
+    if (goc > 360)
+        goc = goc - 360;
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y){
+    switch (button){
+        case GLUT_LEFT_BUTTON:
+            if(state == GLUT_DOWN)
+                glutIdleFunc(spinDisplay);
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            if(state == GLUT_DOWN)
+                glutIdleFunc(NULL);
+            break;
+        default:
+            break;
+    }
 }
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(250, 250);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("car");
     init();
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
     glutMainLoop();
     return 0;
 }
