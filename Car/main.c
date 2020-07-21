2406d9c46c57327a2b2b94db42a960f3330dc2dd
Chinh Nguyen <22429215+tchinh97@users.noreply.github.com>
2020-07-21 08:37:27 +0700

    Initial commit

--- Car/main.c
+++ Car/main.c
@@ -1,15 +1,36 @@
-//
-//  main.c
-//  Car
-//
-//  Created by Chinh Nguyen on 7/20/20.
-//  Copyright © 2020 Chinh Nguyen. All rights reserved.
-//
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#include <OpenGL/gl.h>
 
-#include <stdio.h>
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0); //    màu của đối tượng
+//    Thong tin cua hinh chu nhat
+    glBegin(GL_POLYGON);
+        glVertex3f(0.25, 0.25, 0.0);
+        glVertex3f(0.75, 0.25, 0.0);
+        glVertex3f(0.75, 0.75, 0.0);
+        glVertex3f(0.25, 0.75, 0.0);
+    glEnd();
+    glFlush();
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 0.0); //    nền đen
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("car");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
