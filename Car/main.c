74f72304f923fef7ba5b0750ef43e1da254afcfc
Chinh Nguyen <22429215+tchinh97@users.noreply.github.com>
2020-08-03 10:05:01 +0700

    Update main.c

--- Car/main.c
+++ Car/main.c
@@ -1,53 +1,103 @@
-#define GL_SILENCE_DEPRECATION
 #include <GLUT/glut.h>
 #include <OpenGL/glu.h>
 #include <OpenGL/gl.h>
 
-//Góc quay của hình chữ nhật
-static GLfloat goc = 0.0;
+static GLfloat spin = 0;
+static GLfloat move = 0;
 
-void init() {
-//    Nền màu đen
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glShadeModel(GL_FLAT);
+void init(){
+    glClearColor(0, 0, 0, 0);   //black
+//    glClearColor(1, 1, 1, 1);   // white
+}
+
+void reshape(int w, int h){
+//    ma tran phep chieu
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+//    phep chieu phoi canh
+    gluPerspective(40, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(5, 5, 5, 0, 0, 0, 0, 1, 0);
+//    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 }
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
-    glPushMatrix();
     
-//    Xoay quanh trục z
-    glRotatef(goc, 0.0, 0.0, 1.0);
-    glColor3f(1.0, 1.0, 1.0);
-    glRectf(-25.0, -25.0, 25.0, 25.0); /* vẽ hcn */
+//     than xe
+    glPushMatrix();
+    glScalef(2, 1, 1);
+    glTranslatef(move*0.55+0.24, 0.675, 0);
+    glColor3f(1, 0, 0);
+    glutWireCube(1);
     glPopMatrix();
+    
+//     banh xe 1
+    glPushMatrix();
+    glTranslatef(move, 0, 0);
+    glRotatef(spin, 0, 0, 1);
+    glColor3f(0, 1, 1);
+    glutSolidTorus(0.1, 0.2, 10, 10);
+    glPopMatrix();
+    
+//    banh xe 2
+    glPushMatrix();
+    glTranslatef(move, 0, 0.5);
+    glRotatef(spin, 0, 0, 1);
+    glColor3f(0, 1, 1);
+    glutSolidTorus(0.1, 0.2, 10, 10);
+    glPopMatrix();
+    
+//    banh xe 3
+    glPushMatrix();
+    glTranslatef(move + 1, 0, 0);
+    glRotatef(spin, 0, 0, 1);
+    glColor3f(1, 1, 1);
+    glutSolidTorus(0.1, 0.2, 10, 10);
+    glPopMatrix();
+    
+//    banh xe 4
+    glPushMatrix();
+    glTranslatef(move + 1, 0, 0.5);
+    glRotatef(spin, 0, 0, 1);
+    glColor3f(1, 1, 1);
+    glutSolidTorus(0.1, 0.2, 10, 10);
+    glPopMatrix();
+    
     glutSwapBuffers();
+    glFlush();
 }
 
-void reshape(int w, int h) {
-    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-}
-
-void spinDisplay(){
-    goc = goc + 2.0;
-    if (goc > 360)
-        goc = goc - 360;
+// Di chuyen sang phai theo truc Ox
+void spinRight(void) {
+    move = move + 0.01;
+    spin = spin + 2.0;
     glutPostRedisplay();
 }
 
-void mouse(int button, int state, int x, int y){
-    switch (button){
+// Di chuyen sang trai theo truc Ox
+void spinLeft(void){
+    move = move - 0.01;
+    spin = spin - 2.0;
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y) {
+    switch (button) {
+//            chuột phải
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN)
+                    glutIdleFunc(spinRight);
+            break;
+//            chuột trái
         case GLUT_LEFT_BUTTON:
             if(state == GLUT_DOWN)
-                glutIdleFunc(spinDisplay);
+                glutIdleFunc(spinLeft);
             break;
+//            nút giữa chuột
         case GLUT_MIDDLE_BUTTON:
-            if(state == GLUT_DOWN)
+            if (state == GLUT_DOWN)
                 glutIdleFunc(NULL);
             break;
         default:
@@ -55,10 +105,12 @@ void mouse(int button, int state, int x, int y){
     }
 }
 
-int main(int argc, char **argv) {
+
+
+int main(int argc, char** argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(250, 250);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+    glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("car");
     init();
